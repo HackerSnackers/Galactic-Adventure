fb47abba3eddb33be63e831da5f00a6b1c7e5299
bbrierr <44116042+bbrierr@users.noreply.github.com>
2018-10-13 14:13:54 -0400

    change id to print to

--- thejsfile.js
+++ thejsfile.js
@@ -11,6 +11,7 @@
 				
 				
 				document.getElementById("scene1").innerHTML = "You leave your computer station and walk down the hallway to the crash, as you get closer to the crash site you start to smell smoke. You were trained to alert the ship immediately but you know if you do this for a false alarm you may be fired from your position and sent back to earth. What do you do?";
+				document.getElementById("story-text-box").innerHTML = "You leave your computer station and walk down the hallway to the crash, as you get closer to the crash site you start to smell smoke. You were trained to alert the ship immediately but you know if you do this for a false alarm you may be fired from your position and sent back to earth. What do you do?";
 
 			}
 			
