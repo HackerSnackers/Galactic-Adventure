8f073e25cfb4fdccf29c174e76f409eee4be7872
bbrierr <44116042+bbrierr@users.noreply.github.com>
2018-10-13 13:55:35 -0400

    renamed buttons

--- thejsfile.js
+++ thejsfile.js
@@ -3,6 +3,15 @@
 		var choicesA456 = new Array("Choice5A ", "Choice5B ", 
 		
 		function clickButton() {
+				document.getElementById("btn1").innerHTML="sound alarm";
+				document.getElementById("btn2").innerHTML="check out the smoke";
+				document.getElementById("btn3").innerHTML="";
+				document.getElementById("btn4").innerHTML="";
+				
+				
+				document.getElementById("scene1").innerHTML = "You leave your computer station and walk down the hallway to the crash, as you get closer to the crash site you start to smell smoke. You were trained to alert the ship immediately but you know if you do this for a false alarm you may be fired from your position and sent back to earth. What do you do?";
+
+			}
+			
+
 		
-		document.getElementById("choie").innerHTML = "You found it!";
-		}
