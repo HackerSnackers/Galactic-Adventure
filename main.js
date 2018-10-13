6e5b04d58ecc22b254236d188ee01652ddb2b76d
hrcarve <44116035+hrcarve@users.noreply.github.com>
2018-10-13 12:51:08 -0400

    Newarrays

---
+++ main.js
@@ -0,0 +1,8 @@
+		var choices1andDone= new Array("Choice1A ", "Choice2AA ", "Choice1B ", "Choice2B ", "Choice2bb ", "ChoiceGameover ", "ChoiceRestart ");
+		var choicesA234 =  new Array("Choice2A ", "Choice3A ", "Choice3AA ", "Choice3AAA ", "Choice3AB ", "Choice4A ", "Choice4AA ");
+		var choicesA456 = new Array("Choice5A ", "Choice5B ", 
+		
+		function clickButton() {
+		
+		document.getElementById("choie").innerHTML = "You found it!";
+		}
