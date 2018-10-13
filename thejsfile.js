a5e2a280b8750c26b55d661c7ec955b2a5c133f9
hrcarve <44116035+hrcarve@users.noreply.github.com>
2018-10-13 14:39:28 -0400

    Y

--- thejsfile.js
+++ thejsfile.js
@@ -1,6 +1,4 @@
 
-		var choices = [1A, 2AA , 1B , 2B , 1C , 1D, 2A, 3A , 3AA , 3AAA , 3AAAA, 4A, 4AA , 5A , 5AA , 6A , 7A , 7AA , 7AAAA , 7AAAA , 8A , 8AA];
-	
 		var A = 13;
 
 		var B = 1000;
@@ -8,7 +6,9 @@
 		var C = 3;
 
 		var D = 0;
-
+		
+		var choices = [1A, 2AA , 1B , 2B , 1C , 1D, 2A, 3A , 3AA , 3AAA , 3AAAA, 4A, 4AA , 5A , 5AA , 6A , 7A , 7AA , 7AAAA , 7AAAA , 8A , 8AA];
+	
 		function clickButton() {
 		document.getElementById("btn1").innerHTML = choices;
 		}
