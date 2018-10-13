e92832323936c7a23fd3513943ca00171f339b96
bbrierr <44116042+bbrierr@users.noreply.github.com>
2018-10-13 16:27:32 -0400

    Update thejsfile.js

--- thejsfile.js
+++ thejsfile.js
@@ -30,10 +30,30 @@
 		}
 
 		function clickButton2(){
-			document.getElementById("btn1").innerHTML = "Succes";
-			document.getElementById("btn2").innerHTML = "Succes";
-			document.getElementById("btn3").innerHTML = "Succes";
-			document.getElementById("btn4").innerHTML = "Succes";
+			if (i==1){
+				document.getElementById("story").appendChild(document.createElement("br"));
+				document.getElementById("story").appendChild(document.createTextNode("You leave your computer station and walk down the hallway to the crash, as you get closer to the crash site you start to smell smoke. You were trained to alert the ship immediately but you know if you do this for a false alarm you may be fired from your position and sent back to earth. What do you do?"));
+			document.getElementById("btn1").innerHTML = "Sound the fire alarm to alert the station";
+			document.getElementById("btn2").innerHTML = "Check out the smoke before sounding the alarm";
+			document.getElementById("btn3").innerHTML = "";
+			document.getElementById("btn4").innerHTML = "";
+			i = 2;
+			}
+		
+			
+			
+			
+			
+			if (i==0){
+				document.getElementById("story").appendChild(document.createElement("br"));
+				document.getElementById("story").appendChild(document.createTextNode("You casually keep working telling yourself that you will check out the crash after the system is updated. Pretty soon you become tired, what do you do?"));
+
+			document.getElementById("btn1").innerHTML = "Drink a coffee";
+			document.getElementById("btn2").innerHTML = "Check out the noise";
+			document.getElementById("btn3").innerHTML = "";
+			document.getElementById("btn4").innerHTML = "";
+			i = 1;
+		}
 		}
 
 		function clickButton3(){
