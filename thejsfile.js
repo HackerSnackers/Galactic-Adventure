7466c6d7cec869fe3fe483bc93bf66dea9c895cb
apmason13 <apmason13@gmail.com>
2018-10-13 15:07:18 -0400

    asd

--- thejsfile.js
+++ thejsfile.js
@@ -1,23 +1,13 @@
-	var A = 13;
-<<<<<<< HEAD
-
-=======
-		
->>>>>>> 91a4a7b26454eff0211359156706a3cc514d6e5b
+		var A = 13;
 		var B = 1000;
-
 		var C = 3;
-
 		var D = 0;
 
-<<<<<<< HEAD
 
 		var choices = [1*A, 2*A*A , 1*B , 2*B , 1*C , 1*D, 2*A, 3*A , 3*A*A , 3*A*A*A , 3*A*A*A*A, 4*A, 4*A*A , 5*A , 5*A*A , 6*A , 7*A , 7*A*A , 7*A*A*A*A , 7*A*A*A*A , 8*A , 8*A*A];
-=======
-		
+
 		var choices = [1*A, 2*A*A , 1*B , 2*B , 1*C , 1*D, 2*A, 3*A , 3*A*A , 3*A*A*A , 3*A*A*A*A, 4*A, 4*A*A , 5*A , 5*A*A , 6*A , 7*A , 7*A*A , 7*A*A*A*A , 7*A*A*A*A , 8*A , 8*A*A];
-	
->>>>>>> 91a4a7b26454eff0211359156706a3cc514d6e5b
+
 
 
 		function clickButton1(){
