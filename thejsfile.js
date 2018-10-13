8ec0c762fb08466acce32d6080c677f25d0688eb
hrcarve <44116035+hrcarve@users.noreply.github.com>
2018-10-13 12:59:43 -0400

    Array done

--- thejsfile.js
+++ thejsfile.js
@@ -1,6 +1,7 @@
-		var choices1andDone= new Array("Choice1A ", "Choice2AA ", "Choice1B ", "Choice2B ", "Choice2bb ", "ChoiceGameover ", "ChoiceRestart ");
-		var choicesA234 =  new Array("Choice2A ", "Choice3A ", "Choice3AA ", "Choice3AAA ", "Choice3AB ", "Choice4A ", "Choice4AA ");
-		var choicesA456 = new Array("Choice5A ", "Choice5B ", 
+		var choices1andDone= new Array("Choice1A ", "Choice2AA ", "Choice1B ", "Choice2B ", "ChoiceGameover ", "ChoiceRestart ");
+		var choices23 =  new Array("Choice2A ", "Choice3A ", "Choice3AA ", "Choice3AAA ", "Choice3AAAA");
+		var choices456 = new Array("Choice4A ", "Choice4AA ", "Choice5A ", "Choice5AA ", "Choice6A ");
+		var choices78 = new Array("Choice7A ", "Choice7AA ", "Choice7AAAA ", "Choice7AAAA ", "Choice8A ", "Choice8AA ");
 		
 		function clickButton() {
 		
