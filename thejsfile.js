6ef124e71543395fb913c1152dfbf0f9314ca78e
hrcarve <44116035+hrcarve@users.noreply.github.com>
2018-10-13 14:25:25 -0400

    ASDF

--- thejsfile.js
+++ thejsfile.js
@@ -8,21 +8,6 @@
 		var C = 3;
 
 		var D = 0;
-<<<<<<< HEAD
-	
-		
-		function clickButton() {
-<<<<<<< HEAD
-				document.getElementById("btn1").innerHTML="sound alarm";
-				document.getElementById("btn2").innerHTML="check out the smoke";
-				document.getElementById("btn3").innerHTML="";
-				document.getElementById("btn4").innerHTML="";
-				
-				
-				document.getElementById("scene").innerHTML = "You leave your computer station and walk down the hallway to the crash, as you get closer to the crash site you start to smell smoke. You were trained to alert the ship immediately but you know if you do this for a false alarm you may be fired from your position and sent back to earth. What do you do?";
-=======
->>>>>>> 88d27f85f1f6a42f515f338067688f79c9db705e
-
 
 		function clickButton() {
 		document.getElementById("btn1").innerHTML = choices;
