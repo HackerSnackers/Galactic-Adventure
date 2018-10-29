bed5cb537dedbd396454991713b7c94e1d84e807
Nathan-Binkley <nbink2@gmail.com>
2018-10-28 20:01:41 -0400

    Push to gh-pages for web access

---
+++ runthrough.js
@@ -0,0 +1,40 @@
+var A = 13;
+var B = 1000;
+var C = 3;
+var D = 0;
+
+
+var choices = [1*A, 2*A*A , 1*B , 2*B , 1*C , 1*D, 2*A, 3*A , 3*A*A , 3*A*A*A , 3*A*A*A*A, 4*A, 4*A*A , 5*A , 5*A*A , 6*A , 7*A , 7*A*A , 7*A*A*A*A , 7*A*A*A*A , 8*A , 8*A*A];
+
+var choices = [1*A, 2*A*A , 1*B , 2*B , 1*C , 1*D, 2*A, 3*A , 3*A*A , 3*A*A*A , 3*A*A*A*A, 4*A, 4*A*A , 5*A , 5*A*A , 6*A , 7*A , 7*A*A , 7*A*A*A*A , 7*A*A*A*A , 8*A , 8*A*A];
+
+
+
+function clickButton1(){
+  document.getElementById("btn1").innerHTML = "Succes";
+  document.getElementById("btn2").innerHTML = "Succes";
+  document.getElementById("btn3").innerHTML = "Succes";
+  document.getElementById("btn4").innerHTML = "Succes";
+
+}
+
+function clickButton2(){
+  document.getElementById("btn1").innerHTML = "Succes";
+  document.getElementById("btn2").innerHTML = "Succes";
+  document.getElementById("btn3").innerHTML = "Succes";
+  document.getElementById("btn4").innerHTML = "Succes";
+}
+
+function clickButton3(){
+  document.getElementById("btn1").innerHTML = "Succes";
+  document.getElementById("btn2").innerHTML = "Succes";
+  document.getElementById("btn3").innerHTML = "Succes";
+  document.getElementById("btn4").innerHTML = "Succes";
+}
+
+function clickButton4(){
+  document.getElementById("btn1").innerHTML = "Succes";
+  document.getElementById("btn2").innerHTML = "Succes";
+  document.getElementById("btn3").innerHTML = "Succes";
+  document.getElementById("btn4").innerHTML = "Succes";
+}
